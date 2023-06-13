7334d3c25b10916b27fd2ab6e04b278559b64aa8
Nick Horvath <nhorvath@gmail.com>
2023-06-13 14:21:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pyrebase4',
-    version='4.7.0',
+    version='4.7.1',
     url='https://github.com/nhorvath/Pyrebase4',
     description='A simple python wrapper for the Firebase API with current deps',
     author='nhorvath',
@@ -16,8 +16,9 @@ setup(
     keywords='Firebase',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'requests>=2.19.1',
-        'requests_toolbelt>=0.7.1',
+        'requests-toolbelt>=0.7.1,<1.0',
+        'requests>=2.19.1,<2.30',
+        'urllib3>=1.21.1,<2',
         'gcloud>=0.18.3',
         'oauth2client>=4.1.2',
         'python-jwt>=2.0.1',
